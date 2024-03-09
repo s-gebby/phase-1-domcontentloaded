e9ee17de3d57e5b0581f7606e59e4488d73b4196
silas <silasgebhart12@gmail.com>
2024-03-09 15:05:18 -0600

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  var messageElement = document.getElementById("message");
+  if (messageElement) {
+    messageElement.textContent = "This is really cool!";
+  }
+});
